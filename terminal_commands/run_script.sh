56caf8e54e20c6214a33480d7302ffcf720c1f5e
mrwrongwrongwrong <26257959+mrwrongwrongwrong@users.noreply.github.com>
2021-07-16 21:47:07 -0400

    Create run_script.sh

---
+++ terminal_commands/run_script.sh
@@ -0,0 +1,10 @@
+https://askubuntu.com/questions/38661/how-do-i-run-sh-scripts
+
+---give execution permission to my script:
+chmod +x /path/to/yourscript.sh
+
+---run my script:
+/path/to/yourscript.sh
+or 
+./yourscript.sh
+
